689d1828e998d998bc250d3063aeb4e64e9d1447
Vishaal Jayavelu <vishaal.j.trs@gmail.com>
2023-08-09 01:22:50 +0530

    Deleting

--- server/controllers/home.controller.js
+++
@@ -1,5 +0,0 @@
-function gethome(req,res){
-     res.status(200).send('hello world');
-}
-
-module.exports = {gethome};
--- server/routes/home.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const {gethome}= require('../controllers/home.controller');
-
-const homeRouter = express.Router();
-
-homeRouter.get('/',gethome);
-
-module.exports = homeRouter;
